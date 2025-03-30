260dde60cfcd090e8282d9edfb962d3dbd5f490c
nakita628 <nakita@ypm.llc>
2025-03-30 06:17:14 +0000

    test fix

--- packages/hekireki/src/generator/mermaid/index.test.ts
+++ packages/hekireki/src/generator/mermaid/index.test.ts
@@ -16,9 +16,9 @@ describe("mermaid", () => {
 
 		const expected = `\`\`\`mermaid
 erDiagram
-    User ||--o{ Post : "(id) - (userId)"
-    Post ||--o{ Like : "(id) - (postId)"
-    User ||--o{ Like : "(id) - (userId)"
+    User ||--|{ Post : "(id) - (userId)"
+    Post ||--|{ Like : "(id) - (postId)"
+    User ||--|{ Like : "(id) - (userId)"
     User {
         String id "Unique identifier for the user."
         String username "Username of the user."
