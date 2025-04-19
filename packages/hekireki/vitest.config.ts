db0425b34165027223a76a22650e231795a8abe5
nakita628 <nakita@ypm.llc>
2025-04-19 08:50:08 +0000

    test setting and fmt

--- packages/hekireki/vitest.config.ts
+++ packages/hekireki/vitest.config.ts
@@ -4,7 +4,15 @@ export default defineConfig({
   test: {
     exclude: ['**/dist/**', '**/src/type/*.ts', '**/node_modules/**', '**/vitest.config.ts'],
     coverage: {
-      exclude: ['**/src/**/*.test.ts', '**/dist/**', '**/vitest.config.ts'],
+      exclude: [
+        '**/src/**/*.test.ts',
+        '**/dist/**',
+        '**/vitest.config.ts',
+        '**/prisma/valibot/*',
+        '**/prisma/zod/*',
+        '**/common/type/*',
+        '**/generator/mermaid-er/type',
+      ],
       all: true,
     },
   },
