8c8f62d1165789094891c51b72530d5618ac88bb
nakita628 <nakita@ypm.llc>
2025-06-29 07:58:23 +0000

    update package

---
+++ packages/hekireki/src/shared/types.ts
@@ -0,0 +1,19 @@
+import type { DMMF } from '@prisma/generator-helper'
+
+export type Field = {
+  documentation: string
+  modelName: string
+  fieldName: string
+  comment: string[]
+  validation: string | null
+}
+
+export type ValidField = Required<Field>
+
+export type GroupedFields = Record<string, Field[]>
+
+export type ModelInfo = {
+  documentation: string
+  name: string
+  fields: unknown
+}
