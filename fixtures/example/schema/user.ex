a34bc27727448edb0d4839fe2de8f2547b4b8cfb
nakita628 <nakita@ypm.llc>
2025-08-26 05:11:10 +0000

    add

--- fixtures/example/schema/user.ex
+++ fixtures/example/schema/user.ex
@@ -1,8 +1,14 @@
 defmodule DBSchema.User do
   use Ecto.Schema
-  @primary_key false
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @type t :: %__MODULE__{
+          id: Ecto.UUID.t(),
+          name: String.t()
+        }
+
   schema "user" do
-    field(:id, :binary_id, primary_key: true)
     field(:name, :string)
   end
 end
