60c8b01af898d7c058997ba4b2220f9a3879a6ed
nakita628 <takibilabdev@gmail.com>
2026-06-05 08:11:06 +0000

    fix

--- fixtures/no-annotation/drizzle/schema.ts
+++ fixtures/no-annotation/drizzle/schema.ts
@@ -1,5 +1,5 @@
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 import { relations, sql } from 'drizzle-orm'
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
 export const user = sqliteTable('user', {
   id: text('id')
